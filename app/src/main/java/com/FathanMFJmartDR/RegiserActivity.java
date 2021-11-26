8480363177db29e71ffc3a67a30e8fe14f91dacd
Fathan Malik <fathan.malik91@ui.ac.id>
2021-11-26 21:58:11 +0700

    tp modul 9

---
+++ app/src/main/java/com/FathanMFJmartDR/RegiserActivity.java
@@ -0,0 +1,14 @@
+package com.FathanMFJmartDR;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegiserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_regiser);
+    }
+}
